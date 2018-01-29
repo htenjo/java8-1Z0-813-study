0998222c86396eea27f0056c2ae617c4ab69f05f
Hernan <zerovirus23@gmail.com>
2018-01-28 21:59:19 -0500

    3. Localization and DateTime

--- src/test/java/co/zero/ocp/LocalizationTest.java
+++ src/test/java/co/zero/ocp/LocalizationTest.java
@@ -1,4 +1,70 @@
 package co.zero.ocp;
 
+import org.junit.Test;
+
 public class LocalizationTest {
+    @Test
+    public void useLocale() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLocalWithResourceBundle() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLocalDate() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLocalTime() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLocalDateTime() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useInstant() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void usePeriod() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useDuration() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFormattersToFormatNumbers() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFormattersToFormatDates() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFormattersToFormatCurrency() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useDatesAndTimesInDifferentTimeZones() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useDateAndTimeWIthDaylightSaving() {
+        throw new UnsupportedOperationException();
+    }
 }
