d4e7a60b3cd43ef113b335be6913535c067c3779
Hernan <zerovirus23@gmail.com>
2018-01-28 22:48:05 -0500

    4. NIO 2 & Readme

--- src/test/java/co/zero/ocp/JavaNio2Test.java
+++ src/test/java/co/zero/ocp/JavaNio2Test.java
@@ -1,4 +1,91 @@
 package co.zero.ocp;
 
+import org.junit.Test;
+
 public class JavaNio2Test {
+    @Test
+    public void usePathsClassToOperateOverFiles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void usePathsClassToOperateOverDirectories() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToCheckFiles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToDeleteFiles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToCopyFiles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToMoveFiles() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToCheckDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToDeleteDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToCopyDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesClassToMoveDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useDirectoryStreamToRecursiveAccessInDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFileVisitorToRecursiveAccessInDirectory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void usePathMatcherToFindAFile() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFiledFind() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesWalk() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFilesLines() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useWatchServiceToObserveChangesInDirectory() {
+        throw new UnsupportedOperationException();
+    }
 }
+
