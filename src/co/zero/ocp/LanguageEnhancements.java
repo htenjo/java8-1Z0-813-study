4aae725dee0738a9f7ac3a7e66c8db1d4080337c
Hernan <zerovirus23@gmail.com>
2018-01-28 21:08:39 -0500

    1. Language Enhancements

---
+++ src/co/zero/ocp/LanguageEnhancements.java
@@ -0,0 +1,42 @@
+package co.zero.ocp;
+
+public class LanguageEnhancements {
+    public static String useStringInSwitch(String param) {
+        switch (param) {
+            case "Option 1":
+                return "True";
+            case "Option 2":
+                return "False";
+            default:
+                return null;
+        }
+    }
+
+    public static void useBinaryLiterals() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void useNumericLiterals() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void useTryCatchwithResources() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void useMultipleExceptionHandling() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void useStaticMethodsOnInterfaces() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void useDefaultMethodsOnInterfaces() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void useDefaultMethodOnInterfaceWithInheritance() {
+        throw new UnsupportedOperationException();
+    }
+}
