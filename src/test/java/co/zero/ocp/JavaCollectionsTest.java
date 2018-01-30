64be2928004bf8618cc049db4371f765fee21fed
Hernan <zerovirus23@gmail.com>
2018-01-29 21:39:57 -0500

    Requirements to understand news on Collections

--- src/test/java/co/zero/ocp/JavaCollectionsTest.java
+++ src/test/java/co/zero/ocp/JavaCollectionsTest.java
@@ -1,4 +1,130 @@
 package co.zero.ocp;
 
+import org.junit.Test;
+
 public class JavaCollectionsTest {
+    @Test
+    public void useDiamondSyntaxWithGenerics() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCollectionIteratorsWithLambdas() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void sueCollectionFiltersWithLambdas() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCollectionSortingWithLambdas() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFindFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFindAny() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useAnyMatch() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useAllMatch() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useNoneMatch() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCalculationsWithCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCalculationsWithMax() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCalculationsWithMin() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCalculationsWithAverage() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCalculationsWithSum() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useAverageInDouble() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useGroupingBy() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useJoining() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void usePartitioningBy() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useCollectionsRemoveIf() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useListReplaceAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useMapComputeIfAbsent() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useMapComputeIfPresent() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useStreamMerge() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useStreamFlatMap() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useStreamMap() {
+        throw new UnsupportedOperationException();
+    }
 }
