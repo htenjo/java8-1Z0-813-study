73e10674496f94b8758ae7b268617f00e8b5c3f4
Hernan <zerovirus23@gmail.com>
2018-01-29 21:22:58 -0500

    Requirements to understand lambdas

--- src/test/java/co/zero/ocp/LambdaTest.java
+++ src/test/java/co/zero/ocp/LambdaTest.java
@@ -1,4 +1,56 @@
 package co.zero.ocp;
 
+import org.junit.Test;
+
 public class LambdaTest {
+    @Test
+    public void useFunctionalInterfaces() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useJavaUtilFunctionPackage() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLambdasToReplaceAnonymousClasses() {
+        //describe type inference and target typing
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useFunctionInterface() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useConsumerInterface() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useSupplierInterface() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useUnaryOperatos() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void usePredicates() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useOptionalApi() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useMethodReference() {
+        throw new UnsupportedOperationException();
+    }
 }
