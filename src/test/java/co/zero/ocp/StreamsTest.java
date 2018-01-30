dd7492251d6abfe32df02b54b4689ff88ec2bfdb
Hernan <zerovirus23@gmail.com>
2018-01-29 21:44:38 -0500

    Requirements to understand Streams

--- src/test/java/co/zero/ocp/StreamsTest.java
+++ src/test/java/co/zero/ocp/StreamsTest.java
@@ -1,4 +1,40 @@
 package co.zero.ocp;
 
+import org.junit.Test;
+
 public class StreamsTest {
+    @Test
+    public void useStreamInterface() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useArrayStream() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useIntStream() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useLazyLambdaExpressions() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useParallelStreams() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useDecompositionOperations() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void useReductionOperations() {
+        throw new UnsupportedOperationException();
+    }
 }
