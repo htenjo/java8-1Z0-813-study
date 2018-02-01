135e7ebb3879af5f9b3cb59f9863eaeade27fe0c
Hernan <zerovirus23@gmail.com>
2018-01-31 20:57:41 -0500

    Overloading resolution examples II

--- src/main/java/co/zero/ocp/GeneralKnowledge.java
+++ src/main/java/co/zero/ocp/GeneralKnowledge.java
@@ -45,4 +45,85 @@ class GeneralKnowledge {
     static String overloadingDown(Object number) {
         return OBJECT;
     }
+
+
+    /**
+     * Helper classes to create examples of java features
+     */
+    public static class ShapeEqualsWrong {
+        private int x, y;
+
+        public ShapeEqualsWrong(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean equals(ShapeEqualsWrong shape) {
+            return (shape instanceof ShapeEqualsWrong && shape.x == this.x && shape.y == this.y);
+        }
+    }
+
+    public static class ShapeEqualsOk {
+        private int x, y;
+
+        public ShapeEqualsOk(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean equals(Object shape) {
+            return (shape instanceof ShapeEqualsOk
+                    && ((ShapeEqualsOk) shape).x == this.x
+                    && ((ShapeEqualsOk) shape).y == this.y);
+        }
+    }
+
+    public static class Shape {
+        private int x, y;
+
+        public Shape(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public String getName(int param) {
+            return Shape.class.getSimpleName() + param;
+        }
+
+        public String getWrapperName(Long param) {
+            return Shape.class.getSimpleName() + "Long";
+        }
+    }
+
+    public static class Circle extends Shape {
+        public Circle(int x, int y) {
+            super(x, y);
+        }
+
+        public String getName(int param) {
+            return "OverrideOK";
+        }
+
+        public String getName(Integer param) {
+            return "OverrideInteger";
+        }
+
+        public String getName(Object param) {
+            return "OverrideObject";
+        }
+    }
+
+    public static class Triangle extends Shape {
+        public Triangle(int x, int y) {
+            super(x, y);
+        }
+
+        public String getWrapperName(Long param) {
+            return "OverrideOK";
+        }
+
+        public String getWrapperName(long param) {
+            return "OverrideLowerLong";
+        }
+    }
 }
--- src/test/java/co/zero/ocp/GeneralLKnowledgeTest.java
+++ src/test/java/co/zero/ocp/GeneralLKnowledgeTest.java
@@ -3,18 +3,17 @@ package co.zero.ocp;
 import org.junit.Test;
 
 import static co.zero.ocp.GeneralKnowledge.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class GeneralLKnowledgeTest {
     @Test
-    public void verifyConstructor() {
+    public void constructor_works_whenNewIsCall() {
         GeneralKnowledge instance = new GeneralKnowledge();
         assertNotNull(instance);
     }
 
     @Test
-    public void verifyOverloadingResolutionTypes() {
+    public void overloading_resolveUp_whenUsingDifferentTypes() {
         String stringResult = GeneralKnowledge.overloadingPrint("9");
         String intResult = GeneralKnowledge.overloadingPrint(9);
 
@@ -34,7 +33,7 @@ public class GeneralLKnowledgeTest {
     }
 
     @Test
-    public void verifyOverloadingUp() {
+    public void overloading_resolveUp_whenThereIsNotExactType() {
         String intResult = GeneralKnowledge.overloadingUp(1);
         String stringResult = GeneralKnowledge.overloadingUp("Something");
 
@@ -43,9 +42,53 @@ public class GeneralLKnowledgeTest {
     }
 
     @Test
-    public void verifyOverloadingDown() {
+    public void overloading_resolveUp_whenThereIsLowerTypes() {
         String intRetult = GeneralKnowledge.overloadingDown(1);
 
         assertEquals(OBJECT, intRetult);
     }
+
+    @Test
+    public void override_hiddenParentMethod_whenChildMethodUseCovariantParameters() {
+        ShapeEqualsWrong shape1 = new ShapeEqualsWrong(1, 2);
+        ShapeEqualsWrong shape2 = new ShapeEqualsWrong(1, 2);
+        Object shape3 = new ShapeEqualsWrong(1, 2);
+        Object shape4 = shape1;
+
+        assertTrue(shape1.equals(shape2)); //Not overriding, just using the method that hides the equals from Object
+        assertFalse(shape1.equals(shape3)); //As parameter Object, then using the equals from Object
+        assertFalse(shape3.equals(shape1)); //Static type is Object, dynamic is Shape1, however equals is not...
+        assertFalse(shape3.equals(shape4)); //overridden then equals from Object is used
+
+    }
+
+    @Test
+    public void override_works_whenChildMethodSignatureIsSameAsParent() {
+        ShapeEqualsOk shape1 = new ShapeEqualsOk(1, 2);
+        ShapeEqualsOk shape2 = new ShapeEqualsOk(1, 2);
+        Object shape3 = new ShapeEqualsOk(1, 2);
+
+        assertTrue(shape1.equals(shape2));
+        assertTrue(shape1.equals(shape3));
+        assertTrue(shape3.equals(shape1));
+    }
+
+    @Test
+    public void override_works_whenExactSignature() {
+        Circle circle1 = new Circle(1, 1);
+        Shape shape1 = circle1;
+        Integer param = new Integer(1);
+        Object objectParam = param;
+        Triangle triangle1 = new Triangle(1, 2);
+        Shape shape2 = triangle1;
+
+        assertTrue(circle1.getName(1) == "OverrideOK");
+        assertTrue(shape1.getName(1) == "OverrideOK");
+        assertTrue(circle1.getName(param) == "OverrideInteger"); //Not override
+        assertTrue(circle1.getName(objectParam) == "OverrideObject"); //Not override
+
+        assertTrue(triangle1.getWrapperName(new Long(1)) == "OverrideOK");
+        assertTrue(triangle1.getWrapperName(1L) == "OverrideLowerLong"); //Not override
+    }
 }
+
